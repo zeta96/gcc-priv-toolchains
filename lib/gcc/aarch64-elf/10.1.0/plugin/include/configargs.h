040da6ec1ed888aa9eff448ddaaeaf93d39d5da9
zeta96 <ahmad.zainul857@gmail.com>
2020-05-16 10:06:54 +0000

    [update]: GCC-10.1.0-binutils-2.34

---
+++ lib/gcc/aarch64-elf/10.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/runner/gcc/build/gcc-10.1.0/configure --target=aarch64-elf --enable-targets=all --prefix=/home/runner/gcc/10.1.0 --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
